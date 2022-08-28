d215241b647007c7724db1f21a39b9996010c88e
matthew-botten <86299176+matthew-botten@users.noreply.github.com>
2022-08-28 19:03:51 +0200

    Update latest.py

--- latest.py
+++ latest.py
@@ -1,2 +1,7 @@
 from time import sleep
 import sklearn
+Import numpy
+import matplotlib.pyplot as plt
+randomNumbers = numpy.random.uniform(0.0, 5.0, 100000)
+plt.hist(randomNumbers, 100)
+plt.show()
